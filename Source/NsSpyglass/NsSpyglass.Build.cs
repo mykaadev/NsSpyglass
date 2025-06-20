f15a4464e99b19a1a3d6825cb6b90e0de5323fbb
mykaa <micaelsilva.code@gmail.com>
2025-06-20 12:00:50 +0100

    Fixed some 5.2 deprecated functions

--- Source/NsSpyglass/NsSpyglass.Build.cs
+++ Source/NsSpyglass/NsSpyglass.Build.cs
@@ -4,50 +4,53 @@ using UnrealBuildTool;
 
 public class NsSpyglass : ModuleRules
 {
-	public NsSpyglass(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
+    public NsSpyglass(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicIncludePaths.AddRange(
+            new string[]
+            {
+                // ... add public include paths required here ...
+            }
+        );
+
+
+        PrivateIncludePaths.AddRange(
+            new string[]
+            {
+                // ... add other private include paths required here ...
+            }
+        );
+
+
         PublicDependencyModuleNames.AddRange(
-                new string[]
-                {
-                        "Core"
-                }
-                );
+            new string[]
+            {
+                "Core"
+            }
+        );
 
 
         PrivateDependencyModuleNames.AddRange(
-                new string[]
-                {
-                        "CoreUObject",
-                        "Engine",
-                        "Slate",
-                        "SlateCore",
-                        "UnrealEd",
-                        "ToolMenus"
-                }
-                );
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
-	}
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "UnrealEd",
+                "ToolMenus",
+                "Projects",
+            }
+        );
+
+
+        DynamicallyLoadedModuleNames.AddRange(
+            new string[]
+            {
+                // ... add any modules that your module loads dynamically here ...
+            }
+        );
+    }
 }
--- Source/NsSpyglass/Private/Widgets/SSpyglassGraphWidget.cpp
+++ Source/NsSpyglass/Private/Widgets/SSpyglassGraphWidget.cpp
@@ -19,17 +19,17 @@ void SSpyglassGraphWidget::BuildNodes() const
     {
         FPluginNode Node;
         Node.Name = Plugin->GetName();
-        if (const FPluginDescriptor* Desc = Plugin->GetDescriptor())
+        const FPluginDescriptor& Desc = Plugin->GetDescriptor();
+
+        for (const FPluginReferenceDescriptor& Ref : Desc.Plugins)
         {
-            for (const FPluginReferenceDescriptor& Ref : Desc->Plugins)
+            if (Ref.bEnabled)
             {
-                if (Ref.bEnabled)
-                {
-                    Node.Dependencies.Add(Ref.Name);
-                }
+                Node.Dependencies.Add(Ref.Name);
             }
         }
-        float Angle = (2.f * PI) * Index / Plugins.Num();
+
+        const float Angle = (2.f * PI) * Index / Plugins.Num();
         Node.Position = Center + FVector2D(FMath::Cos(Angle) * Radius, FMath::Sin(Angle) * Radius);
         Nodes.Add(Node);
         ++Index;
@@ -67,15 +67,16 @@ int32 SSpyglassGraphWidget::OnPaint(const FPaintArgs& Args, const FGeometry& All
         FSlateDrawElement::MakeBox(
             OutDrawElements,
             LayerId + 1,
-            AllottedGeometry.ToPaintGeometry(DrawPos, FVector2D(50.f, 50.f)),
+            AllottedGeometry.ToPaintGeometry(FVector2D(50.f, 50.f), FSlateLayoutTransform(DrawPos)),
             FCoreStyle::Get().GetBrush("WhiteBrush"),
             ESlateDrawEffect::None,
             FLinearColor::Blue
         );
+
         FSlateDrawElement::MakeText(
             OutDrawElements,
             LayerId + 2,
-            AllottedGeometry.ToPaintGeometry(DrawPos + FVector2D(5.f, 15.f), FVector2D(40.f,20.f)),
+            AllottedGeometry.ToPaintGeometry(FVector2D(40.f,20.f), FSlateLayoutTransform(DrawPos + FVector2D(5.f, 15.f))),
             Node.Name,
             FCoreStyle::Get().GetFontStyle("NormalFont"),
             ESlateDrawEffect::None,
